540ae1fe9172ebb206b2a3785139adcea12f928c
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2025-04-02 21:26:33 +0100

    Remove Vec output from benchmarks
    There's pretty much no difference between it and io::Empty,
    and we don't really care about measuring Vec performance or allocations,
    so let's remove it, as it only clutters the benchmark results.

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,5 +1,6 @@
 use std::num::NonZeroUsize;
 use std::{io, iter};
+
 use tinytable::write_table;
 
 #[cfg(tinytable_profile_alloc)]
@@ -16,47 +17,47 @@ fn main() {
 const COLUMNS: usize = 10;
 const COLUMN_WIDTHS: [NonZeroUsize; COLUMNS] = [NonZeroUsize::new(10).unwrap(); COLUMNS];
 
-#[divan::bench(args = [10, 100, 1000, 10000], types = [Vec<u8>, io::Empty])]
-fn no_padding<W: io::Write + Default>(rows: usize) {
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn no_padding(rows: usize) {
     const S: &str = "          ";
     const ROW: [&str; COLUMNS] = [S; COLUMNS];
 
-    let mut output: W = Default::default();
+    let mut output = io::empty();
     write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
 }
 
-#[divan::bench(args = [10, 100, 1000, 10000], types = [Vec<u8>, io::Empty])]
-fn no_padding_unicode<W: io::Write + Default>(rows: usize) {
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn no_padding_unicode(rows: usize) {
     const S: &str = "あいうえお";
     const ROW: [&str; COLUMNS] = [S; COLUMNS];
 
-    let mut output: W = Default::default();
+    let mut output = io::empty();
     write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
 }
 
-#[divan::bench(args = [10, 100, 1000, 10000], types = [Vec<u8>, io::Empty])]
-fn all_padding<W: io::Write + Default>(rows: usize) {
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn all_padding(rows: usize) {
     const S: &str = "";
     const ROW: [&str; COLUMNS] = [S; COLUMNS];
 
-    let mut output: W = Default::default();
+    let mut output = io::empty();
     write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
 }
 
-#[divan::bench(args = [10, 100, 1000, 10000], types = [Vec<u8>, io::Empty])]
-fn single_padding<W: io::Write + Default>(rows: usize) {
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn single_padding(rows: usize) {
     const S: &str = "        ";
     const ROW: [&str; COLUMNS] = [S; COLUMNS];
 
-    let mut output: W = Default::default();
+    let mut output = io::empty();
     write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
 }
 
-#[divan::bench(args = [10, 100, 1000, 10000], types = [Vec<u8>, io::Empty])]
-fn too_long<W: io::Write + Default>(rows: usize) {
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn too_long(rows: usize) {
     const S: &str = "                ";
     const ROW: [&str; COLUMNS] = [S; COLUMNS];
 
-    let mut output: W = Default::default();
+    let mut output = io::empty();
     write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
 }
