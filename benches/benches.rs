88878e9f67ec8d07cb98b18596628e7b83e75abc
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2025-04-02 22:08:10 +0100

    Add benchmarks for write_table_with_fmt and write_table_fallible

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,7 +1,7 @@
 use std::num::NonZeroUsize;
 use std::{io, iter};
 
-use tinytable::write_table;
+use tinytable::{write_table, write_table_with_fmt};
 
 #[cfg(tinytable_profile_alloc)]
 use divan::AllocProfiler;
@@ -51,6 +51,87 @@ fn single_padding(rows: usize) {
 
     let mut output = io::empty();
     write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
+}
+
+#[cfg(feature = "fallible-iterator")]
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn single_padding_fallible(rows: usize) {
+    use fallible_iterator::FallibleIterator;
+    use tinytable::write_table_fallible;
+
+    const S: &str = "        ";
+    const ROW: [&str; COLUMNS] = [S; COLUMNS];
+
+    let mut output = io::empty();
+    write_table_fallible(
+        &mut output,
+        fallible_iterator::repeat::<_, ()>(ROW).take(rows),
+        &ROW,
+        &COLUMN_WIDTHS,
+    )
+    .expect("write_table failed");
+}
+
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn single_padding_unicode(rows: usize) {
+    const S: &str = "あいうえ";
+    const ROW: [&str; COLUMNS] = [S; COLUMNS];
+
+    let mut output = io::empty();
+    write_table(&mut output, iter::repeat_n(ROW, rows), &ROW, &COLUMN_WIDTHS).expect("write_table failed");
+}
+
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn single_padding_fmt_reuse_cell(rows: usize) {
+    const ROW: &str = "        ";
+    const COLUMN_NAMES: [&str; COLUMNS] = [ROW; COLUMNS];
+
+    fn fmt(row: &&str, f: &mut String) -> std::fmt::Result {
+        use std::fmt::Write;
+        write!(f, "{}", row)
+    }
+
+    let mut output = io::empty();
+    write_table_with_fmt(
+        &mut output,
+        iter::repeat_n(ROW, rows),
+        &[fmt; COLUMNS],
+        &COLUMN_NAMES,
+        &COLUMN_WIDTHS,
+    )
+    .expect("write_table failed");
+}
+
+#[divan::bench(args = [10, 100, 1000, 10000])]
+fn single_padding_fmt_visitor(rows: usize) {
+    const S: &str = "        ";
+    const ROW: [&str; COLUMNS] = [S; COLUMNS];
+
+    fn fmt(index: usize) -> impl Fn(&[&str; COLUMNS], &mut String) -> std::fmt::Result {
+        use std::fmt::Write;
+        move |row: &[&str; COLUMNS], f: &mut String| write!(f, "{}", row[index])
+    }
+
+    let mut output = io::empty();
+    write_table_with_fmt(
+        &mut output,
+        iter::repeat_n(ROW, rows),
+        &[
+            fmt(0),
+            fmt(1),
+            fmt(2),
+            fmt(3),
+            fmt(4),
+            fmt(5),
+            fmt(6),
+            fmt(7),
+            fmt(8),
+            fmt(9),
+        ],
+        &ROW,
+        &COLUMN_WIDTHS,
+    )
+    .expect("write_table failed");
 }
 
 #[divan::bench(args = [10, 100, 1000, 10000])]
